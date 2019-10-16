c8847ba9ad683a20325f88bd7b4557aa704920cf
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-10-15 19:15:47 -0700

    added delete UI to the dashboard

--- client/src/components/BusinessPortal/Promotions/Promotions.js
+++ client/src/components/BusinessPortal/Promotions/Promotions.js
@@ -1,56 +1,56 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 // material-ui
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-
-
-import { createMuiTheme } from '@material-ui/core/styles';
-
-import PromotionForm from '../PromotionForm/PromotionForm'
+import PromotionForm from "../PromotionForm/PromotionForm";
 
 // components
-import Title from '../Title/Title';
+import Title from "../Title/Title";
+import DeletePromotion from '../DeletePromotion/DeletePromotion'
 
 // constructor for creating mock data
 function createData(id, name, qtypeople, description) {
-    return { id, name, qtypeople, description };
+  return { id, name, qtypeople, description };
 }
 
 // generate mock data
 const rows = [
-    createData(0, 'Cinco De Mayo', '15% off on all beers', 10),
-    createData(1, 'Independence Day', 'Buy 1 get 1 free', 15),
-    createData(2, 'Super Bowl', '1 Free Appetizer', 5),
-    createData(3, 'Bootcamp Graduation Party', '25% off drinks with student ID', 8),
-    createData(4, 'Jay-Z\'s Birthday Party', 'Free valet parking', 4)
+  createData(0, "Cinco De Mayo", "15% off on all beers", 10),
+  createData(1, "Independence Day", "Buy 1 get 1 free", 15),
+  createData(2, "Super Bowl", "1 Free Appetizer", 5),
+  createData(
+    3,
+    "Bootcamp Graduation Party",
+    "25% off drinks with student ID",
+    8
+  ),
+  createData(4, "Jay-Z's Birthday Party", "Free valet parking", 4)
 ];
 
 // styling
 const useStyles = makeStyles(theme => ({
-    // promo bottom
-    promoBottom: {
-        marginTop: theme.spacing(3),
-    },
-    // icon sizes
-    fab: {
-        margin: theme.spacing(1)
-    },
-    marginIcon: {
-        margin: theme.spacing(1),
-    },
-    extendedIcon: {
-        marginRight: theme.spacing(1),
-    },
+  // promo bottom
+  promoBottom: {
+    marginTop: theme.spacing(3)
+  },
+  // icon sizes
+  fab: {
+    margin: theme.spacing(1)
+  },
+  marginIcon: {
+    margin: theme.spacing(1)
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
 }));
 
 // trying to add button color; may need to theme the entire page
@@ -65,64 +65,63 @@ const useStyles = makeStyles(theme => ({
 // });
 
 const addPromotion = () => {
-    console.log('add promo');
+  console.log("add promo");
 };
-
-const deletePromotion = () => {
-    console.log('delete promo');
-};
-
-
 
 // render promotions
 export default function Promotions() {
-    const classes = useStyles();
-    const [formActive, setActive] = useState(false);
+  const classes = useStyles();
+  const [formActive, setActive] = useState(false);
 
-    return (
-        <React.Fragment>
+  return (
+    <React.Fragment>
+      <Title>My Promotions</Title>
 
-            <Title>My Promotions</Title>
+      <Table size="medium">
+        {/* columns */}
+        <TableHead>
+          <TableRow>
+            <TableCell>name</TableCell>
+            <TableCell>description</TableCell>
+            <TableCell>qtypeople</TableCell>
+            {/* <TableCell align="right">Some buttons</TableCell> */}
+          </TableRow>
+        </TableHead>
 
-            <Table size="medium">
-
-                {/* columns */}
-                <TableHead>
-                    <TableRow>
-                        <TableCell>name</TableCell>
-                        <TableCell>description</TableCell>
-                        <TableCell>qtypeople</TableCell>
-                        {/* <TableCell align="right">Some buttons</TableCell> */}
-                    </TableRow>
-                </TableHead>
-
-                {/* promo information */}
-                <TableBody>
-
-                    {/* mapo through promo response and append data to promo table */}
-                    {rows.map(row => (
-                        <TableRow key={row.id}>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.qtypeople}</TableCell>
-                            <TableCell>{row.description}</TableCell>
-                            <TableCell align="right"><DeleteForeverIcon  id={"delete_" + row.id}/></TableCell>
-                        </TableRow>
-                    ))}
-
-                </TableBody>
-
-            </Table>
-            <div className={classes.promoBottom}>
-                <Fab size="small" variant="extended" aria-label="delete" className={classes.fab}       onClick={() => {
+        {/* promo information */}
+        <TableBody>
+          {/* mapo through promo response and append data to promo table */}
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.qtypeople}</TableCell>
+              <TableCell>{row.description}</TableCell>
+              <TableCell align="right">
+                <DeletePromotion id={"delete_" + row.id} promoName={row.name}/>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className={classes.promoBottom}>
+        <Fab
+          size="small"
+          variant="extended"
+          aria-label="delete"
+          className={classes.fab}
+          onClick={() => {
             formActive ? setActive(false) : setActive(true);
-          }}>
-                    <AddIcon className={classes.extendedIcon} />
-                    Add Promotion
-                </Fab>
-                {formActive ? <PromotionForm formContents= {addPromotion}/> : <span></span>}
-
-            </div>
-        </React.Fragment>
-
-    );
+          }}
+        >
+          <AddIcon className={classes.extendedIcon} />
+          Add Promotion
+        </Fab>
+        {formActive ? (
+          <PromotionForm formContents={addPromotion} />
+        ) : (
+          <span></span>
+        )}
+      </div>
+    </React.Fragment>
+  );
 }
