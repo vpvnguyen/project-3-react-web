1797a326c0c6a2b3e9847c71fa701bdb3932ea1d
Alex Hwong <violinmana@gmail.com>
2019-10-24 20:55:37 -0700

    changed from aws ip back to localhost

--- client/src/components/BusinessPortal/AddBusiness/AddBusinessPage/AddBusiness.jsx
+++ client/src/components/BusinessPortal/AddBusiness/AddBusinessPage/AddBusiness.jsx
@@ -63,7 +63,7 @@ class AddBusinessPage extends Component {
     let user = this.props.user
     pendingBusiness.user_id = user[0].id
     axios
-        .post('http://3.132.214.172/api/addBusiness', pendingBusiness)
+        .post('http://localhost/api/addBusiness', pendingBusiness)
         .then(result => {
             console.log(result)
         })
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.static('development'))
 //setting cors headers to allow react app to hit REST API 
 app.use(
     cors({
-      origin: "http://3.132.214.172", // allow to server to accept request from different origin
+      origin: "http://localhost:3000", // allow to server to accept request from different origin
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true // allow session cookie from browser to pass through
     })
